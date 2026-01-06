4860cd6cf080dfe70d45ef477ee492eb8ec84b1e
Said Aroua <said.aroua@bluewin.ch>
2026-01-06 13:15:58 +0100

    Remove unused deps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -147,15 +147,6 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-local js_ts_formatters = function()
-	-- If we find a biome.json config in project root, assume biome
-	if vim.uv.fs_stat("./biome.json") then
-		return { "biome" }
-	else
-		return { "prettierd", "prettier", stop_after_first = true }
-	end
-end
-
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -611,7 +602,6 @@ require("lazy").setup({
 						},
 					},
 				},
-				biome = {},
 
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 
